2521da09abd273b55c7e0041e668590a248b3f16
Yuki Nagato <foxnewsnetwork@gmail.com>
2012-04-12 23:56:15 -0700

    minor fixes to the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Appleseed::Application.routes.draw do
   resources :users
 
-  get "pages/home"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,7 +50,7 @@ Appleseed::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
