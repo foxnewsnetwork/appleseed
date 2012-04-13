ed9ee623e2cdf39e174a25868d9122232d94d5f8
Yuki Nagato <foxnewsnetwork@gmail.com>
2012-04-12 23:42:52 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appleseed::Application.config.secret_token = 'a8bc51f6ef9355366e3bffc41deac762e6f5311a491fe278c8866a1316ae069c95a992e8f6f3ce915573ec8dbbccdd78d44b86eebb726a3cf3fd1cb5b8c939a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Appleseed::Application.config.session_store :cookie_store, :key => '_appleseed_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Appleseed::Application.config.session_store :active_record_store
